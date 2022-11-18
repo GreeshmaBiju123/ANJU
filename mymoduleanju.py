a0fc8816eaf6361e94e0e87ac39fd38bb3d06ffb
GreeshmaBiju123 <93526448+GreeshmaBiju123@users.noreply.github.com>
2022-11-17 22:16:41 -0700

    Add files via upload

---
+++ mymoduleanju.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# create a module
+def my_function(name):
+    print("Hello" + name)
+
+
+# In[2]:
+
+
+# Save this code in a file named mymodule.py
+
+
+# In[3]:
+
+
+## Variables in Module
+person1={"name":"ammu","age":34,"country":"india"}
+
